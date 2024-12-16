07ebf8b25b4fd0300f01aa3598b91fbccd0f2d38
mahmoud <mahmoudamged00@gmail.com>
2024-12-16 18:55:41 +0200

    end commit

--- js/script.js
+++ js/script.js
@@ -1,19 +1,25 @@
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 4, // Show 3 cards at once
-    spaceBetween: 30, // Space between slides
-    centeredSlides: true, // Center the active slide
-    loop: true, // Enable infinite scrolling
-    autoplay: {
-      delay: 2500, // Autoplay delay (2.5 seconds)
-      disableOnInteraction: false, // Continue autoplay after interaction
-    },
-    pagination: {
-      el: ".swiper-pagination", // Pagination element
-      clickable: true, // Allow clicking on pagination bullets
-    },
-    navigation: {
-      nextEl: ".swiper-button-next", // Navigation to next slide
-      prevEl: ".swiper-button-prev", // Navigation to previous slide
-    },
+
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+      loop: true,
+      margin: 10,
+      nav: true,
+      dots:false,
+      autoplay: true,
+      autoplayTimeout: 3000,
+      autoplayHoverPause: true,
+      responsive: {
+          0: {
+              items: 1
+          },
+          600: {
+              items: 2
+          },
+          1000: {
+              items: 4
+          }
+      }
   });
+});
+
 
